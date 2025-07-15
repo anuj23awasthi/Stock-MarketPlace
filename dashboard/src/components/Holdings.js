da4bea88750b943716fddd0d721c2585cf211ee5
Anuj <anujawasthi350@gmail.com>
2025-07-16 01:13:15 +0530

    Chart Functionality added

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,16 +1,48 @@
-import React ,{useState, useEffect} from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import axios, { all } from "axios";
+import { VerticalGraph } from "./VerticalGraph";
 
 // import { holdings } from "../Data/data.js";
 
 const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
+
   useEffect(() => {
-axios.get("http://localhost:3002/allHoldings").then((res) => {
-  console.log(res.data);
+    axios.get("http://localhost:3002/allHoldings").then((res) => {
+      // console.log(res.data);
       setAllHoldings(res.data);
     });
   }, []);
+
+  // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const labels = allHoldings.map((subArray) => subArray["name"]);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Stock Price",
+        data: allHoldings.map((stock) => stock.price),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  // export const data = {
+  //   labels,
+  //   datasets: [
+  // {
+  //   label: 'Dataset 1',
+  //   data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
+  // },
+  //     {
+  //       label: 'Dataset 2',
+  //       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+  //       backgroundColor: 'rgba(53, 162, 235, 0.5)',
+  //     },
+  //   ],
+  // };
 
   return (
     <>
@@ -30,20 +62,20 @@ axios.get("http://localhost:3002/allHoldings").then((res) => {
           </tr>
 
           {allHoldings.map((stock, index) => {
-            const currValue = stock.price * stock.qty;
-            const isProfit = currValue - stock.avg * stock.qty >= 0.0;
+            const curValue = stock.price * stock.qty;
+            const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
             const dayClass = stock.isLoss ? "loss" : "profit";
 
             return (
-              <tr key={index} >
+              <tr key={index}>
                 <td>{stock.name}</td>
                 <td>{stock.qty}</td>
                 <td>{stock.avg.toFixed(2)}</td>
                 <td>{stock.price.toFixed(2)}</td>
-                <td>{currValue.toFixed(2)}</td>
+                <td>{curValue.toFixed(2)}</td>
                 <td className={profClass}>
-                  {(currValue - stock.avg * stock.qty).toFixed(2)}
+                  {(curValue - stock.avg * stock.qty).toFixed(2)}
                 </td>
                 <td className={profClass}>{stock.net}</td>
                 <td className={dayClass}>{stock.day}</td>
@@ -71,6 +103,7 @@ axios.get("http://localhost:3002/allHoldings").then((res) => {
           <p>P&L</p>
         </div>
       </div>
+      <VerticalGraph data={data} />
     </>
   );
 };
